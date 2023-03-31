951a59f03bb8a8078565533245c624cb4d1324ac
freeLGR <110274180+freeLGR@users.noreply.github.com>
2023-03-31 09:28:16 +0800

    Add files via upload

---
+++ homework4/homework1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{    
+     //节点类
+    public class Node<T>
+        {
+            public T Data { get; set; }//每个节点存储的值
+            public Node<T> Next { get; set; }//下一个节点
+            public Node(T t)
+            {
+                Data = t;
+                Next = null;
+            }
+        }
+  
+    
+
+        //泛型链表类
+        public class GenercList<T> { 
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenercList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head { get { return head; } }
+
+            public void Add(T t)
+            {
+                Node<T> n= new Node<T>(t);
+                if(head == null)
+                {
+                    head =tail= n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+           
+
+            public void ForEach(Action<T> action)
+            {
+               Node<T> node = head;//node指向头节点
+                //遍历所有的节点，对其中的node的数据进行赋值
+                while(node != null)
+                {
+                    action(node.Data);
+                    node = node.Next;
+                }
+        }
+
+
+
+    }
+
+
+
+    //    //Main（)类
+    //    public class Program { 
+    //    //Main()方法
+    //    public static void Main()
+    //        {   //创建一个链表，其存放的数据为1，2，3，4
+    //            GenercList<int> list = new GenercList<int>();
+    //            list.Add(1);
+    //            list.Add(5);
+    //            list.Add(4);
+    //            list.Add(9);
+    //            //接着，将调用其ForEach函数，大英每一个值
+    //            list.ForEach((a) => { Console.WriteLine(a); });
+    //            //lambda表达式可以调用局部变量
+    //            int sum = 0;
+    //            int min = list.Head.Data;
+    //            int max = list.Head.Data;
+    //            list.ForEach((a) =>
+    //            {
+    //                min = (min < a) ? min : a;//改变最小值
+    //                max = (max > a) ? max : a;//改变最大值
+    //                sum += a;
+    //            });
+
+
+    //                Console.WriteLine($"max：{max} min:{min} sum：{sum}");
+    //        }
+
+
+    //    }
+
+
+}
---
+++ homework4/homework2.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+//闹钟类
+public class Clock {
+    //属性
+    public int hour;
+    public int minute;
+    public int second;
+    public int alarmHour;//响铃时间
+    public int alarmMinute;
+    public int alarmSecond;
+
+    //委托类型
+    public delegate void aTick();
+    public delegate void aAlarm();
+
+    //事件 还未赋值
+    public event aAlarm Alarm;
+    public event aTick Tick;
+
+    //构造函数
+    public Clock(int hour, int minnite, int second) {
+        this.hour = hour;
+        this.minute = minnite;
+        this.second = second;
+        alarmSecond = 0;
+        alarmMinute = 0;
+        alarmHour = 0;
+    }
+
+    //无参构造函数
+    public Clock() {
+        hour = 0;
+        minute = 0;
+        second = 0;
+        alarmHour = 0;
+        alarmMinute = 0;
+        alarmSecond = 0;
+    }
+
+    //IsTick函数 IsAlarm函数
+    public void IsTick()
+    {
+        Console.WriteLine("da da da da da da");
+    }
+
+    public void IsAlarm() {
+        Console.WriteLine("响铃啦！");
+        
+    }
+
+    //设置响铃时间
+    public bool SetAlarm(int hour, int minnite, int second) {
+        //判断修改时间的合法性
+        if (hour >= 24 || hour < 0 || minnite < 0 || minnite >= 60 || second < 0 || second >= 60) return false;
+        alarmHour = hour;
+        alarmMinute = minute;
+        alarmSecond = second;
+        return true;
+}
+
+
+    //判断时钟的时间是否合法
+    public bool IsValid() {
+        if (hour >= 24 || hour < 0 || minute < 0 || minute >= 60 || second < 0 || second >= 60) return false;
+        return true;
+    }
+
+    //时钟运行
+    public void Run()
+    {
+        if (hour == alarmHour && minute == alarmMinute && second == alarmSecond) {
+            Alarm();
+        }
+        second ++;
+        //将此时的时间合法化,此时前提时，之前的时间是合法
+        if (second >= 60)
+        {
+            second = 0;
+            minute += 1;
+            if(minute >= 60)
+            {
+                minute = 0;
+                hour++;
+                if(hour >= 24)
+                {
+                    hour = 0;
+                }
+            }
+        }
+        //响铃事件触发
+        Tick();
+
+    }
+
+}
+
+
+//程序运行类Main()函数
+
+public class Program {
+    public static void Main(string[] args) { 
+        Clock clock = new Clock();
+        clock.Alarm += clock.IsAlarm;
+        clock.Tick += clock.IsTick;
+        while (true) { 
+        clock.Run();
+        Thread.Sleep(1000);
+        }
+    
+    }
+
+    private static void Clock_Tick()
+    {
+        throw new NotImplementedException();
+    }
+}
